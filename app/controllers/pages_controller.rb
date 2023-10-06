658de49aeaa033ce415c3c602602d2f15a4e2cdd
Syanom <yandupliy@gmail.com>
2023-10-06 15:47:16 +0300

    add show for pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   def index
     @page_hierarchies = Page.root.map { |page| page.get_hierarchy }
   end
+
+  def show
+    @page = Page.find_by(name: params[:pages].split('/').last)
+    @hierarchy = @page.get_hierarchy
+  end
 end

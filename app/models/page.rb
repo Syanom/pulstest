f7ef56257ae2e6580c03c8c2d46ddd7e5f0467e0
Syanom <yandupliy@gmail.com>
2023-10-07 19:21:28 +0300

    add page name validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,7 @@
 class Page < ApplicationRecord
   belongs_to :parent_page, class_name: 'Page', foreign_key: 'parent_page_id', optional: true
   has_many :child_pages, class_name: 'Page', foreign_key: 'parent_page_id', dependent: :nullify
+  validates_format_of :name, with: /\A[a-zA-Z0-9_А-Яа-я]+\z/, message: 'Name must contain only: _ A-z А-я 0-9'
 
   scope :root, -> { where(parent_page_id: nil) }
 

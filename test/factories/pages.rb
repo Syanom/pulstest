de2e19c3a0d9dc28fc5d1465da412439069cc65b
Syanom <yandupliy@gmail.com>
2023-10-09 02:23:50 +0300

    make factory_bot support sequence for page names

--- test/factories/pages.rb
+++ test/factories/pages.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :page do
-    name { 'page_name' }
+    sequence(:name) { |n| "page#{n}" }
     content_raw { "*[строка]*\n\\\\[строка]\\\\\n((name1/name2/name3 [строка]))" }
     content { 'MyText' }
     header { 'page_header' }

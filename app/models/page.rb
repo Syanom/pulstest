c2842b37fde346d6a8fae7c8ed5908f18dbadde6
Syanom <yandupliy@gmail.com>
2023-10-08 19:17:30 +0300

    make content_raw produce content before save

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,9 @@ class Page < ApplicationRecord
   belongs_to :parent_page, class_name: 'Page', foreign_key: 'parent_page_id', optional: true
   has_many :child_pages, class_name: 'Page', foreign_key: 'parent_page_id', dependent: :nullify
 
+  # making replasements to produce content for show and leave content_raw for edit
+  before_save :construct_content
+
   validates_format_of :name, with: /\A[a-zA-Z0-9_А-Яа-я]+\z/, message: 'must contain only: _ A-z А-я 0-9'
   # I duplicate this in database migration in case of we have multiple sources of pages
   validates_uniqueness_of :name, message: 'must be unique'
@@ -37,5 +40,18 @@ class Page < ApplicationRecord
       { name: page.name, path: "#{parent_path}#{page.name}",
         pages: pages[page.id] ? hasify_children(pages, page.id, "#{parent_path}#{page.name}/") : [] }
     end
+  end
+
+  private
+
+  # making replasements to produce content for show and leave content_raw for edit
+  def construct_content
+    # replace *[content]* with <b>content</b>
+    constructed_content = content_raw.gsub(/(\*\[)(.+)(\]\*)/, '<b>\2</b>')
+    # replace //content// with <i>content</i>
+    constructed_content = constructed_content.gsub(/(\\\\)(.+)(\\\\)/, '<i>\2</i>')
+    # replace ((path/to/something content)) with <a href="/path/to/something">content</a>
+    self.content = constructed_content.gsub(%r{(\(\()(.+/.+)( )(.+)(\)\))},
+                                            '<a href="/\2">\4</a>')
   end
 end
